f2cc5273f596a8391a3305f9b9588ba3943acf93
Gere <arguellogeremias234@gmail.com>
2025-11-20 18:42:33 -0300

    correccion de settings

--- PrimerApp/settings.py
+++ PrimerApp/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 import dj_database_url
 
-IS_RENDER = "RENDER" in os.environ
+IS_RENDER = "RENDER_EXTERNAL_HOSTNAME" in os.environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
